b4fbe8727c6f213a7db98a048e5ef1f57316109f
Yann-Eric <yann-eric@live.fr>
2012-11-16 15:53:16 -0800

    Evol : possibilité de mettre un gros cache sur la home page / nécéssité d'adapter la page de parametres

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -18,8 +18,18 @@ class DefaultController extends Controller
         if (is_object($item))
           $images = $this->getDoctrine()->getRepository('DWSlideShowBundle:Image')->findBy(array ('item' => $item->getId()));
         
+        $response = $this->render('DWSlideShowBundle:Default:index.html.twig', array('images' => $images, 'element' => $item));
         
-        return $this->render('DWSlideShowBundle:Default:index.html.twig', array('images' => $images, 'element' => $item));
+        $as_parameter = $this->container->hasParameter('cache_home_page');
+        $cache_home_page = 3600;
+        if ($as_parameter)
+        {
+            if (is_numeric($this->container->getParameter('cache_home_page')))
+            $cache_home_page = $this->container->getParameter('cache_home_page');
+        }
+        $response->setSharedMaxAge($cache_home_page);
+        
+        return $response;
     }
     
 
