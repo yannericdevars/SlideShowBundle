fdf977a409d6e24cc9f1a1b334d12da75c2a2d81
Yann-Eric <yann-eric@live.fr>
2012-11-15 18:39:48 -0800

    Evol: Retire le dernier couplage entre les bundles

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,8 +3,7 @@
 namespace DW\SlideShowBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use DW\UserBundle\Entity\User;
-use Symfony\Component\HttpFoundation\Session\Session;
+
 
 class DefaultController extends Controller
 {
@@ -23,53 +22,6 @@ class DefaultController extends Controller
         return $this->render('DWSlideShowBundle:Default:index.html.twig', array('images' => $images, 'element' => $item));
     }
     
-    public function loginAction(\Symfony\Component\HttpFoundation\Request $request)
-    {
-        
-           $user = new User();
-        
-            $form = $this->createFormBuilder($user)
-                    ->add('username', 'text')
-                    ->add('password', 'text')
-                    ->getForm();
-           
-        
-        if ($request->isMethod('POST')) 
-        {
-            
-            $form->bind($request);
-            
-            $user = $form->getData();
-            
-            $encoded_password = sha1($user->getPassword());
-            
-            $user_log = $this->getDoctrine()->getRepository('DWUserBundle:User')->findOneBy(array ('username' => $user->getUsername(), 'encrypted_password' => $encoded_password));
-            
-            $t_roles = array();
-            
-            if (is_object($user_log))
-            {
-                foreach ($user_log->getRoles() as $role)
-                {
-                    $t_roles[] = $role->getName();
-                }
-            }
-           
-            
-            if (is_object($user_log))
-            {
-                    $this->getRequest()->getSession()->set('userAutentif', $t_roles);
-                
-            }
-            else 
-            {
-               $this->getRequest()->getSession()->set('userAutentif', null);
-            }
-        }
-        
-         return $this->render('DWSlideShowBundle:Default:login.html.twig', array(
-                'form' => $form->createView(),
-            ));     
-    }   
+
 
 }
